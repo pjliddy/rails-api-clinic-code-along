ba38e3b24617aa2b220dea3873a85cc8663be8a8
Samantha Guergenenov <samguergen@gmail.com>
2017-04-19 13:48:35 -0400

    add specialty to curl req

--- scripts/doctors/create-doctor.sh
+++ scripts/doctors/create-doctor.sh
@@ -2,6 +2,7 @@ curl --include --request POST http://localhost:4741/doctors \
   --header "Content-Type: application/json" \
   --data '{
     "doctor": {
+      "specialty": "pediatry",
       "given_name": "Dr",
       "family_name": "Phil",
       "gender": "male"
--- scripts/doctors/update-doctor.sh
+++ scripts/doctors/update-doctor.sh
@@ -2,6 +2,7 @@ curl --include --request PATCH http://localhost:4741/doctors/3 \
   --header "Content-Type: application/json" \
   --data '{
     "doctor": {
+      "specialty": "pediatry",
       "given_name": "Dr",
       "family_name": "Phil",
       "gender": "male"
